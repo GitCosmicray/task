2dff5e98497694864933bf33f74e6425624df497
GitCosmicray <94951603+GitCosmicray@users.noreply.github.com>
2022-01-04 17:47:21 +0530

    Update variable.tf

--- ec2/variable.tf
+++ ec2/variable.tf
@@ -1,15 +1,15 @@
 variable "publicsubnetid" {
-    default = "subnet-0f44ac1bacfdcc23b"
+    default = "subnet-0b067025c93c48433"
 }
 variable "privatesubnetid" {
-    default = "subnet-0f44ac1bacfdcc23b"
+    default = "subnet-0112e39ae96f47298"
 }
 variable "keyname" {
-    default = "cal_key"
+    default = "ohio_key"
 }
 variable "windows_security_group" {
-    default = "sg-087c278e4c51a1523"
+    default = "sg-0c4d1efb671c26929"
 }
 variable "linux_security_group" {
-    default = "sg-087c278e4c51a1523"
+    default = "sg-08431bb922f4b69af"
 }
